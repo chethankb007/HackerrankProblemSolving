f75c5fae986f29fbd0bacbbc92600021a7753dfe
chethankb007 <67217350+chethankb007@users.noreply.github.com>
2020-07-20 14:53:08 +0530

    Updated the Code using Recursion

--- LinkedList/MergeLL.java
+++ LinkedList/MergeLL.java
@@ -64,50 +64,67 @@ public class Solution {
      *
      */
     static SinglyLinkedListNode mergeLists(SinglyLinkedListNode head1, SinglyLinkedListNode head2) {
-        if(head1 == null && head2 == null) return head1;
+        // if(head1 == null && head2 == null) return head1;
+
+        // if(head1 == null) return head2;
+
+        // if(head2 == null) return head1;
+
+        //     SinglyLinkedListNode head = null , tail = null;
+        // if(head1.data <= head2.data)
+        // {
+        //     head = head1;
+        //     tail = head1;
+        //     head1 = head1.next;
+        // }
+        // else
+        // {
+        //     head = head2;
+        //     tail = head2;
+        //     head2 = head2.next;
+
+        // }
+
+        // while(head1 != null && head2 != null)
+        // {
+        //     if(head1.data <= head2.data)
+        //     {
+            
+        //         tail.next= head1;
+        //         tail = tail.next;
+        //         head1 = head1.next;
+        //     }
+        //     else
+        //     {
+        //         tail.next= head2;
+        //         tail = tail.next;
+        //         head2 = head2.next; 
+        //     }
+        // }
+        // if(head1 != null)
+        // tail.next  = head1;
+
+        // if(head2 != null)
+        // tail.next  = head2;
+
+        // return head;
+
+        // Using Recursion
 
         if(head1 == null) return head2;
-
         if(head2 == null) return head1;
 
-            SinglyLinkedListNode head = null , tail = null;
         if(head1.data <= head2.data)
         {
-            head = head1;
-            tail = head1;
-            head1 = head1.next;
+            head1.next = mergeLists(head1.next , head2);
+            return head1;
         }
         else
         {
-            head = head2;
-            tail = head2;
-            head2 = head2.next;
-
+             head2.next = mergeLists(head1, head2.next);
+            return head2;
         }
 
-        while(head1 != null && head2 != null)
-        {
-            if(head1.data <= head2.data)
-            {
-            
-                tail.next= head1;
-                tail = tail.next;
-                head1 = head1.next;
-            }
-            else
-            {
-                tail.next= head2;
-                tail = tail.next;
-                head2 = head2.next; 
-            }
-        }
-        if(head1 != null)
-        tail.next  = head1;
-
-        if(head2 != null)
-        tail.next  = head2;
-
-        return head;
 
 
 
